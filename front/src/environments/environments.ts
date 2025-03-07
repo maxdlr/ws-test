c8f33bd4144667c086dedadb312968c2292439f2
maxdlr <maxdlr@nc-ass-vip.sdv.fr>
2025-03-07 11:09:04 +0100

    make app available on local network

--- front/src/environments/environments.ts
+++ front/src/environments/environments.ts
@@ -2,7 +2,7 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-const serverAddress = 'localhost';
+const serverAddress = window.location.hostname;
 
 export const environment = {
   apiUrl: `http://${serverAddress}:8080/api/`,
